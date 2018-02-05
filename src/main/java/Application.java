720098069a02ae34e830afeebb25165da3f4807b
owels45 <hwjx9wpo03s@vfemail.net>
2018-02-05 18:30:45 +0100

    finished and tested!

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,16 +1,17 @@
-import com.sun.org.apache.regexp.internal.RE;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Application {
     private List<Record> records = new ArrayList<>();
+
+    //this variable should be never touched and always remains the same after initialization (method importCSVFile(String fileName))
+    private List<Record> recordsOriginal;
+
 
     //Strings
     private String strSource;
@@ -19,11 +20,15 @@ public class Application {
     private String strNotSource;
     private Predicate<Record> recordWhereSourceEqualsStrNotSource = r -> !r.getSource().equals(strNotSource);
 
-    private ArrayList<String> listStr = new ArrayList<String>();
-    private Predicate<Record> recordWhereSourceContainsListStr = r -> listStr.contains(r.getSource());
+    private ArrayList<String> sourceList = new ArrayList<String>();
+    private ArrayList<String> destinationList = new ArrayList<String>();
+    private Predicate<Record> recordWhereSourceContainsSourceList = r -> sourceList.contains(r.getSource());
+    private Predicate<Record> recordWhereDestinationContainsDestinationList = r -> destinationList.contains(r.getDestination());
 
-    private ArrayList<String> negativeListStr = new ArrayList<String>();
-    private Predicate<Record> recordWhereSourceNotContainsNegativeListStr = r -> !negativeListStr.contains(r.getSource());
+    private ArrayList<String> negativeSourceList = new ArrayList<String>();
+    private ArrayList<String> negativeDestinationList = new ArrayList<String>();
+    private Predicate<Record> recordWhereSourceNotContainsNegativeSourceList = r -> !negativeSourceList.contains(r.getSource());
+    private Predicate<Record> recordWhereDestinationNotContainsNegativeDestinationList = r -> !negativeDestinationList.contains(r.getDestination());
 
     private String strDestination;
     private Predicate<Record> recordWhereDestinationEqualsStrDestination = r -> r.getDestination().equals(strDestination);
@@ -39,12 +44,17 @@ public class Application {
     private Predicate<Record> recordWhereWeightIsLesserEqualThanIntWeightLET = r -> r.getWeight() <= intWeightLET;
 
     //booleans
-    boolean boolIsExpress;
+    private boolean boolIsExpress;
     private Predicate<Record> recordWhereIsExpressEqualsBoolIsExpress = r -> r.isExpress() == boolIsExpress;
 
-    boolean boolIsValue;
+    private boolean boolIsValue;
     private Predicate<Record> recordWhereIsValueEqualsBoolIsValue = r -> r.isValue() == boolIsValue;
 
+    private boolean boolIsFragile;
+    private Predicate<Record> recordWhereIsFragileEqualsBoolIsFragile = r -> r.isFragile() == boolIsFragile;
+
+    //Comparators
+    private Comparator<Record> orderByWeightDesc = (first, second) -> (int) (second.getWeight() - (first.getWeight()));
 
 
     public void importCSVFile(String fileName) {
@@ -67,6 +77,13 @@ public class Application {
         } catch (IOException ioe) {
             System.out.println(ioe.getMessage());
         }
+        copyRecordsToRecordsOriginal();
+    }
+
+
+    private void copyRecordsToRecordsOriginal() {
+        recordsOriginal = new ArrayList<>(records.size());
+        Collections.copy(records, recordsOriginal);
     }
 
     public List<Record> getRecords() {
@@ -75,11 +92,15 @@ public class Application {
 
     // count
     public long executeSQL01() {
-        return records.stream().count();
+        //return records.stream().count();
+        //entspricht der oberen Zeile, ist aber schneller
+        return (long) records.size();
     }
 
     // count, where
     public long executeSQL02() {
+        copyRecordsToRecordsOriginal();
+
         strSource = "A";
         strDestination = "D";
         Stream<Record> stream = records.stream().filter(recordWhereSourceEqualsStrSource);
@@ -91,11 +112,11 @@ public class Application {
         strSize = "S";
         Stream<Record> stream = records.stream().filter(recordWhereSizeEqualsStrSize);
 
-        listStr.clear();
-        listStr.add("A");
-        listStr.add("B");
-        listStr.add("C");
-        stream = stream.filter(recordWhereSourceContainsListStr);
+        sourceList.clear();
+        sourceList.add("A");
+        sourceList.add("B");
+        sourceList.add("C");
+        stream = stream.filter(recordWhereSourceContainsSourceList);
 
         intWeightGET = 1000;
         stream = stream.filter(recordWhereWeightIsGreaterEqualThanIntWeightGET);
@@ -114,10 +135,10 @@ public class Application {
         strSize = "L";
         Stream<Record> stream = records.stream().filter(recordWhereSizeEqualsStrSize);
 
-        negativeListStr.clear();
-        negativeListStr.add("A");
-        negativeListStr.add("B");
-        stream = stream.filter(recordWhereSourceNotContainsNegativeListStr);
+        negativeSourceList.clear();
+        negativeSourceList.add("A");
+        negativeSourceList.add("B");
+        stream = stream.filter(recordWhereSourceNotContainsNegativeSourceList);
 
         intWeightGET = 1250;
         stream = stream.filter(recordWhereWeightIsGreaterEqualThanIntWeightGET);
@@ -135,35 +156,138 @@ public class Application {
     }
 
     // id, where, in, order by desc limit
-    public void executeSQL05() {
+    public ArrayList<Integer> executeSQL05() {
+        strSize = "S";
+        Stream<Record> stream = records.stream().filter(recordWhereSizeEqualsStrSize);
+
+        destinationList.clear();
+        destinationList.add("D");
+        destinationList.add("E");
+        destinationList.add("F");
+        stream = stream.filter(recordWhereDestinationContainsDestinationList);
+
+        strSource = "B";
+        stream = stream.filter(recordWhereSourceEqualsStrSource);
+
+        intWeightGET = 3500;
+        stream = stream.filter(recordWhereWeightIsGreaterEqualThanIntWeightGET);
+
+        boolIsExpress = true;
+        stream = stream.filter(recordWhereIsExpressEqualsBoolIsExpress);
+
+        boolIsValue = true;
+        stream = stream.filter(recordWhereIsValueEqualsBoolIsValue);
+
+        stream = stream.sorted(orderByWeightDesc).limit(3);
+
+        return stream.map(Record::getId).collect(Collectors.toCollection(ArrayList<Integer>::new));
     }
 
     // id, where, in, order by desc, order by asc
-    public void executeSQL06() {
+    public ArrayList<Integer> executeSQL06() {
+        strSize = "M";
+        Stream<Record> stream = records.stream().filter(recordWhereSizeEqualsStrSize);
+
+        strDestination = "C";
+        stream = stream.filter(recordWhereDestinationEqualsStrDestination);
+
+        sourceList.clear();
+        sourceList.add("G");
+        sourceList.add("H");
+        sourceList.add("I");
+        sourceList.add("J");
+        stream = stream.filter(recordWhereSourceContainsSourceList);
+
+        intWeightLET = 1500;
+        stream = stream.filter(recordWhereWeightIsLesserEqualThanIntWeightLET);
+
+        boolIsExpress = true;
+        stream = stream.filter(recordWhereIsExpressEqualsBoolIsExpress);
+
+        boolIsValue = true;
+        stream = stream.filter(recordWhereIsValueEqualsBoolIsValue);
+
+        Comparator<Record> comparator = orderByWeightDesc.thenComparing(Comparator.comparing(Record::getDestination));
+
+        return stream.sorted(comparator).map(Record::getId).collect(Collectors.toCollection(ArrayList<Integer>::new));
     }
 
     // count, group by
-    public void executeSQL07() {
+    public Map<Boolean, Long> executeSQL07() {
+        return records.stream().collect(Collectors.groupingBy(Record::isExpress, Collectors.counting()));
     }
 
     // count, where, group by
-    public void executeSQL08() {
+    public Map<String, Long> executeSQL08() {
+        boolIsExpress = true;
+        boolIsValue = false;
+        return records.stream().filter(recordWhereIsExpressEqualsBoolIsExpress)
+                .filter(recordWhereIsValueEqualsBoolIsValue)
+                .collect(Collectors.groupingBy(Record::getSize, Collectors.counting()));
     }
 
     // count, where, in, group by
-    public void executeSQL09() {
+    public Map<String, Long> executeSQL09() {
+        destinationList.clear();
+        destinationList.add("A");
+        destinationList.add("C");
+        boolIsValue = false;
+        return records.stream().filter(recordWhereDestinationContainsDestinationList)
+                .filter(recordWhereIsValueEqualsBoolIsValue)
+                .collect(Collectors.groupingBy(Record::getDestination, Collectors.counting()));
     }
 
     // count, where, not in, group by
-    public void executeSQL10() {
+    public Map<String, Long> executeSQL10() {
+        negativeSourceList.clear();
+        negativeSourceList.add("B");
+        negativeSourceList.add("C");
+        boolIsExpress = true;
+        boolIsValue = true;
+        boolIsFragile = true;
+
+        return records.stream().filter(recordWhereSourceNotContainsNegativeSourceList)
+                .filter(recordWhereIsExpressEqualsBoolIsExpress)
+                .filter(recordWhereIsValueEqualsBoolIsValue)
+                .filter(recordWhereIsFragileEqualsBoolIsFragile)
+                .collect(Collectors.groupingBy(Record::getDestination, Collectors.counting()));
     }
 
     // sum, where, not in, in, group by
-    public void executeSQL11() {
+    public Map<Boolean, Integer> executeSQL11() {
+        strSource = "B";
+        negativeDestinationList.clear();
+        negativeDestinationList.add("D");
+        negativeDestinationList.add("E");
+        intWeightGET = 1000;
+        intWeightLET = 2750;
+        return records.stream().filter(recordWhereSourceEqualsStrSource)
+                .filter(recordWhereDestinationNotContainsNegativeDestinationList)
+                .filter(recordWhereWeightIsGreaterEqualThanIntWeightGET)
+                .filter(recordWhereWeightIsLesserEqualThanIntWeightLET)
+                .collect(Collectors.groupingBy(Record::isValue, Collectors.summingInt(Record::getWeight)));
     }
 
     // avg, where, in, in, group by
-    public void executeSQL12() {
+    public Map<String, Integer> executeSQL12() {
+        sourceList.clear();
+        sourceList.add("A");
+        sourceList.add("B");
+
+        destinationList.clear();
+        destinationList.add("C");
+        destinationList.add("D");
+        intWeightGET = 1250;
+        boolIsFragile = true;
+        Stream<Record> stream = records.stream().filter(recordWhereSourceContainsSourceList)
+                .filter(recordWhereDestinationContainsDestinationList)
+                .filter(recordWhereWeightIsGreaterEqualThanIntWeightGET)
+                .filter(recordWhereIsFragileEqualsBoolIsFragile);
+
+        Map<String, Double> map = stream.collect(Collectors.groupingBy(Record::getDestination, Collectors.averagingInt(Record::getWeight)));
+        Map<String, Integer> mapInt = new HashMap<>();
+        for (String key : map.keySet()) mapInt.put(key, map.get(key).intValue());
+        return mapInt;
     }
 
     public void execute() {
@@ -183,7 +307,7 @@ public class Application {
     }
 
     public static void main(String... args) {
-//        Application app = new Application();
-//        app.execute();
+        Application app = new Application();
+        app.execute();
     }
 }
--- src/test/java/ApplicationTest.java
+++ src/test/java/ApplicationTest.java
@@ -1,8 +1,10 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.function.Predicate;
+import java.util.Map;
 
 import static org.junit.Assert.*;
 
@@ -38,34 +40,75 @@ public class ApplicationTest {
 
     @Test
     public void executeSQL05() throws Exception {
+        List<Integer> list = new ArrayList<>();
+        list.add(127_555);
+        list.add(309_059);
+        list.add(263_230);
+        assertEquals(list, app.executeSQL05());
     }
 
     @Test
     public void executeSQL06() throws Exception {
+        List<Integer> list = new ArrayList<>();
+        list.add(468_257);
+        list.add(154_270);
+        assertEquals(list, app.executeSQL06());
     }
 
     @Test
     public void executeSQL07() throws Exception {
+        Map<Boolean, Long> map = new HashMap<>();
+        map.put(Boolean.FALSE, 919_981L);
+        map.put(Boolean.TRUE, 80_019L);
+        assertEquals(map, app.executeSQL07());
     }
 
     @Test
     public void executeSQL08() throws Exception {
+        Map<String, Long> map = new HashMap<>();
+        map.put("S", 26_551L);
+        map.put("M", 26_337L);
+        map.put("L", 26_309L);
+        assertEquals(map, app.executeSQL08());
     }
 
     @Test
     public void executeSQL09() throws Exception {
+        Map<String, Long> map = new HashMap<>();
+        map.put("C", 98_830L);
+        map.put("A", 99_281L);
+        assertEquals(map, app.executeSQL09());
     }
 
     @Test
     public void executeSQL10() throws Exception {
+        Map<String, Long> map = new HashMap<>();
+        map.put("I", 3L);
+        map.put("J", 3L);
+        map.put("G", 3L);
+        map.put("H", 1L);
+        map.put("D", 3L);
+        map.put("B", 1L);
+        map.put("C", 2L);
+        map.put("F", 2L);
+        map.put("A", 1L);
+        assertEquals(map, app.executeSQL10());
     }
 
     @Test
     public void executeSQL11() throws Exception {
+        Map<Boolean, Integer> map = new HashMap<>();
+        map.put(Boolean.FALSE, 56_055_016);
+        map.put(Boolean.TRUE, 558_159);
+        assertEquals(map, app.executeSQL11());
     }
 
     @Test
     public void executeSQL12() throws Exception {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("D", 3_095);
+        map.put("C", 3_185);
+        assertEquals(map, app.executeSQL12());
     }
 
 }
